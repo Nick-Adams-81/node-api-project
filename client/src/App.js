849a18b4c8c3b0fc0f24b9dbd768670d4b3efce4
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-03 16:35:27 -0700

    changed colors of letters

--- client/src/App.js
+++ client/src/App.js
@@ -16,15 +16,15 @@ function App() {
   }, [input])
 
   return (
-    <div className="App" style={{ backgroundColor: "grey" }}>
-      <h1 style={{ marginLeft: "600px", marginTop: "100px", fontSize: "50px" }}>
+    <div className="App" style={{ backgroundColor: "#ECDEEA" }}>
+      <h1 style={{ marginLeft: "600px", marginTop: "20px", fontSize: "50px" }}>
         <span style={{ color: "green" }}>S</span>
         <span style={{ color: "red" }}>P</span>
-        <span style={{ color: "yellow" }}>O</span>
+        <span style={{ color: "#C1CB4F" }}>O</span>
         <span style={{ color: "blue" }}>O</span>
         <span style={{ color: "orange" }}>F</span>
         <span style={{ color: "purple" }}>L</span>
-        <span style={{ color: "yellow" }}>E</span>
+        <span style={{ color: "#C1CB4F" }}>E</span>
       </h1>
 
       <div style={{ marginLeft: "600px" }}>
@@ -34,7 +34,6 @@ function App() {
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <div>Youre input is {input}</div>
         <button>Find info!</button>
       </div>
     </div>
