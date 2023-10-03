3e8054d22f60806e3987767741c4397a76280dca
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-03 15:43:02 -0700

    capturing user input in a variable

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: "grey" }}>
+      <h1 style={{ marginLeft: "600px", marginTop: "100px", fontSize: "50px" }}>
+        <span style={{ color: "green" }}>S</span>
+        <span style={{ color: "red" }}>P</span>
+        <span style={{ color: "yellow" }}>O</span>
+        <span style={{ color: "blue" }}>O</span>
+        <span style={{ color: "orange" }}>F</span>
+        <span style={{ color: "purple" }}>L</span>
+        <span style={{ color: "yellow" }}>E</span>
+      </h1>
+
+      <div style={{ marginLeft: "600px" }}>
+        <input
+          type="text"
+          id="user-input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <div>Youre input is {input}</div>
+        <button>Find info!</button>
+      </div>
     </div>
   );
 }
