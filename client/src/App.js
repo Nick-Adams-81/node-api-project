1347783150efc317154819086e63136156030eaf
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-04 12:14:34 -0700

    getting data based on what user types into input

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function App() {
   const [input, setInput] = useState("");
@@ -9,11 +11,11 @@ function App() {
 
   const dataArray = data.results;
   if (dataArray !== undefined) {
-    console.log(dataArray[1]);
+    console.log(dataArray);
   }
 
   useEffect(() => {
-    fetch(`http://localhost:5000/data/${input}/3`, {
+    fetch(`http://localhost:5000/data/${input}/5`, {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -24,46 +26,88 @@ function App() {
       .catch((error) => console.log(error));
   }, [input]);
 
-  return (
-    <div className="App">
-      <div style={{ backgroundColor: "#ECDEEA" }}>
-        <h1
-          style={{ marginLeft: "600px", marginTop: "20px", fontSize: "50px" }}
-        >
-          <span style={{ color: "green" }}>S</span>
-          <span style={{ color: "red" }}>P</span>
-          <span style={{ color: "#C1CB4F" }}>O</span>
-          <span style={{ color: "blue" }}>O</span>
-          <span style={{ color: "orange" }}>F</span>
-          <span style={{ color: "purple" }}>L</span>
-          <span style={{ color: "#C1CB4F" }}>E</span>
-        </h1>
-        <div style={{ marginLeft: "600px" }}>
-          <input
-            type="text"
-            id="user-input"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
+  if (dataArray === undefined) {
+    return (
+      <div className="App">
+        <div style={{ backgroundColor: "#ECDEEA" }}>
+          <h1
+            style={{ marginLeft: "600px", marginTop: "20px", fontSize: "50px" }}
+          >
+            <span style={{ color: "green" }}>S</span>
+            <span style={{ color: "red" }}>P</span>
+            <span style={{ color: "#C1CB4F" }}>O</span>
+            <span style={{ color: "blue" }}>O</span>
+            <span style={{ color: "orange" }}>F</span>
+            <span style={{ color: "purple" }}>L</span>
+            <span style={{ color: "#C1CB4F" }}>E</span>
+          </h1>
+          <div style={{ marginLeft: "470px", marginBottom: "45px" }}>
+            <input
+              style={{ width: "50%", height: "35px" }}
+              type="text"
+              id="user-input"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
         </div>
       </div>
-      
-      <Row>
-        <Col xs={12} sm={12} md={6} lg={4}>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="App">
+        <div style={{ backgroundColor: "#ECDEEA" }}>
+          <h1
+            style={{ marginLeft: "600px", marginTop: "20px", fontSize: "50px" }}
+          >
+            <span style={{ color: "green" }}>S</span>
+            <span style={{ color: "red" }}>P</span>
+            <span style={{ color: "#C1CB4F" }}>O</span>
+            <span style={{ color: "blue" }}>O</span>
+            <span style={{ color: "orange" }}>F</span>
+            <span style={{ color: "purple" }}>L</span>
+            <span style={{ color: "#C1CB4F" }}>E</span>
+          </h1>
+          <div style={{ marginLeft: "470px", marginBottom: "15px" }}>
+            <input
+              style={{ width: "50%", height: "35px" }}
+              type="text"
+              id="user-input"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <Row>
+          <Col xs={12} sm={12} md={6} lg={4}>
+            {dataArray.map(({ id, title, url, author }) => {
+              return (
+                <Card
+                  style={{
+                    width: "24rem",
+                    margin: "auto",
+                    marginTop: "15px",
+                    border: "1px solid black",
+                    padding: "5px",
+                  }}
+                  key={id}
+                >
+                  <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Nav.Item>
+                      <Nav.Link href={url}>link to story</Nav.Link>
+                    </Nav.Item>
+                    <Card.Text>By: {author}</Card.Text>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default App;
