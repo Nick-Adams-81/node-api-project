a3b6d8dbf6f201b7f1d1e83024283c9f33abdfac
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-05 18:44:44 -0700

    added get route for getting data by id

--- server/routes/api.meta.js
+++ server/routes/api.meta.js
@@ -4,23 +4,22 @@ const router = require("express").Router();
 const url = "https://api.metaphor.systems/search";
 
 const options = (HTTPMethod, input, num) => ({
-    method: HTTPMethod,
-      headers: {
-        accept: "application/json",
-        "content-type": "application/json",
-        "x-api-key": process.env.API_KEY,
-      },
-      body: JSON.stringify({
-        numResults: num,
-        query: `${input}`,
-        useAutoprompt: false,
-      }),
-})
+  method: HTTPMethod,
+  headers: {
+    accept: "application/json",
+    "content-type": "application/json",
+    "x-api-key": process.env.API_KEY,
+  },
+  body: JSON.stringify({
+    numResults: num,
+    query: `${input}`,
+    useAutoprompt: false,
+  }),
+});
 
 router.post("/data/:input/:num?", async (req, res, next) => {
   try {
-    const { input } = req.params;
-    let { num } = req.params;
+    let { input, num } = req.params;
     num = Number(num);
     num > 5 ? (num = 5) : num;
     const data = await fetch(url, options("POST", input, num));
@@ -28,6 +27,23 @@ router.post("/data/:input/:num?", async (req, res, next) => {
     res.json(response);
   } catch (err) {
     next(err);
+  }
+});
+
+router.get("/data/:id", async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const data = await fetch(`https://api.metaphor.systems/contents/${id}`, {
+      headers: {
+        accept: "application/json",
+        "x-api-key": process.env.API_KEY,
+      },
+    });
+    const response = await data.json();
+    console.log(response)
+    res.json(response);
+  } catch (error) {
+    next(error);
   }
 });
 
