713380db77381ab5f013844e5aa0e46c723705a0
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-08 14:13:06 -0700

    added functioning card component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
+import MyCard from "./components/MyCard";
 
 function App() {
   const [input, setInput] = useState("");
@@ -86,33 +87,7 @@ function App() {
         <Row>
           <Col xs={12} sm={12} md={6} lg={4}>
             {dataArray.map(({ id, title, url, author }) => {
-              if (author === null) {
-                author = "unknown";
-              }
-              return (
-                <Card
-                  style={{
-                    width: "24rem",
-                    margin: "auto",
-                    marginTop: "15px",
-                    border: "1px solid black",
-                    padding: "5px",
-                  }}
-                  key={id}
-                >
-                  <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Nav.Item>
-                      <Nav.Link href={url}>link to story</Nav.Link>
-                    </Nav.Item>
-                    <Card.Text>By: {author}</Card.Text>
-                    {/* // TODO : create button that gets data based on id */}
-                    <Button variant="primary" onClick={() => getDataById(id)}>
-                      Get Info
-                    </Button>
-                  </Card.Body>
-                </Card>
-              );
+              return MyCard(id, title, url, author);
             })}
           </Col>
         </Row>
---
+++ client/src/components/MyCard.js
@@ -0,0 +1,37 @@
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+
+const MyCard = (id, title, url, author) => {
+  if (author === null) {
+    author = "unknown";
+  }
+  return (
+    <>
+      <Card
+        style={{
+          width: "24rem",
+          margin: "auto",
+          marginTop: "15px",
+          border: "1px solid black",
+          padding: "5px",
+        }}
+        key={id}
+      >
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Nav.Item>
+            <Nav.Link href={url}>link to story</Nav.Link>
+          </Nav.Item>
+          <Card.Text>By: {author}</Card.Text>
+          {/* // TODO : create button that gets data based on id */}
+          <Button variant="primary" onClick={() => console.log(id)}>
+            Get Info
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default MyCard;
