95369001416b31898d3109c8a49a7111aa78b903
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-09 18:11:56 -0700

    added get request to ui

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,10 @@ import MyInput from "./components/MyInput";
 function App() {
   const [input, setInput] = useState("");
   const [data, setData] = useState([]);
+  const [onePieceOfData, setOnePieceOfData] = useState([]);
 
   const dataArray = data.results;
+  console.log(onePieceOfData)
 
   useEffect(() => {
     fetch(`http://localhost:5000/data/${input}/5`, {
@@ -21,6 +23,13 @@ function App() {
       .then((data) => setData(data))
       .catch((error) => console.log(error));
   }, [input]);
+
+  const getDataById = (id) => {
+    fetch(`http://localhost:5000/data/'${id}'`)
+      .then((res) => res.json())
+      .then((data) => setOnePieceOfData(data.contents[0]))
+      .catch((error) => console.log(error));
+  };
 
   if (dataArray === undefined) {
     return (
@@ -35,7 +44,7 @@ function App() {
         <Row>
           <Col xs={12} sm={12} md={6} lg={4}>
             {dataArray.map(({ id, title, url, author }) => {
-              return MyCard(id, title, url, author);
+              return MyCard(id, title, url, author, getDataById);
             })}
           </Col>
         </Row>
